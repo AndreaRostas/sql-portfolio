9669f72654e4ead3224df28e119c44b32067dc23
AndreaRostas <andrea.rostas58@gmail.com>
2026-07-22 14:25:52 +0200

    Update 01_create_tables.sql

--- project-01-online-shop/01_create_tables.sql
+++ project-01-online-shop/01_create_tables.sql
@@ -1,1 +1,26 @@
 
+-- ==========================================
+-- Project 1 - Online Shop
+-- Create Tables
+-- ==========================================
+
+CREATE TABLE customers (
+    customer_id INTEGER PRIMARY KEY,
+    name VARCHAR(100),
+    city VARCHAR(100)
+);
+
+CREATE TABLE products (
+    product_id INTEGER PRIMARY KEY,
+    product_name VARCHAR(100),
+    category VARCHAR(100),
+    price DECIMAL(10,2)
+);
+
+CREATE TABLE orders (
+    order_id INTEGER PRIMARY KEY,
+    customer_id INTEGER,
+    product_id INTEGER,
+    quantity INTEGER,
+    order_date DATE
+);
